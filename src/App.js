ffb29977b8aae23cd6e32fba90331f6a470b3115
Dakotah Walker <dwalker@contactaac.com>
2018-11-28 08:32:34 -0800

    updates

--- src/App.js
+++ src/App.js
@@ -1,28 +1,52 @@
+/* eslint react/no-did-mount-set-state: 0  no-unused-vars: 0*/
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import logger from 'redux-logger'
+import thunk from 'redux-thunk'
+import { save, load } from 'redux-localstorage-simple'
+
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
+import rootReducer from './rootReducer'
+
+import MoviesList from './movies/MoviesList';
+import MovieDetail from './movies/MovieDetail';
+import Toggle from './toggle/Toggle'
+
+const middleware = [logger, thunk]
+
+const store = createStore(
+  rootReducer,
+  load(),
+  composeWithDevTools(applyMiddleware(...middleware, save())),
+);
+
+const App = () => (
+  <Provider store={store}>
+    <Router>
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Link to="/">
+            <img src={logo} className="App-logo" alt="logo" />
+          </Link>
         </header>
+        <Toggle />
+        <Switch>
+          <Route exact path="/" component={MoviesList} />
+          <Route path="/:id" component={MovieDetail} />
+        </Switch>
       </div>
-    );
-  }
-}
+    </Router>
+  </Provider>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+// First Store
+// const counter = (state = 'hello') => ({ welcome: state });
+// const store = createStore(counter);
+// console.log(store.getState());
+
+// First Reducer & Actions
+// const counter = (state = 'Hello World', action) => {
+//   switch (action.type) {
+//   case 'HI':
+//     return 'Hi Scott';
+//   case 'HELLO':
+//     return 'Hello, how are you?';
+//   default:
+//     return 'Hello world';
+//   }
+// };
+// const store = createStore(counter);
+// store.dispatch({
+//   type: 'HELLO',
+// });
+// console.log(store.getState());
+
+// Second Reducer & Actions
+// const counter = (state = { greeting: 'hi', otherState: 'some stuff' }, action) => {
+//   switch (action.type) {
+//   case 'HI':
+//     return { ...state, greeting: `Hi, ${action.name}` };
+//   case 'HELLO':
+//     return { ...state, greeting: `Hello ${action.name}, how are you?'` };
+//   default:
+//     return { ...state, greeting: 'Hello world' };
+//   }
+// };
+// const store = createStore(counter);
+// store.dispatch({
+//   type: 'HELLO',
+//   name: 'Scott',
+// });
+// console.log(store.getState());
